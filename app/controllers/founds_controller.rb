f7ec377acefc50bcb3ab802062fec7c535b1a483
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-10-01 19:05:41 -0400

    I hope I didn't delete anything

--- app/controllers/founds_controller.rb
+++ app/controllers/founds_controller.rb
@@ -2,47 +2,47 @@ class FoundsController < ApplicationController
     
    before_action :set_found, only: [:show, :update, :destroy]
 
- # GET /founds
- def index
-  @founds = Found.all
-  render(json: @founds) #relationships done for you!
- end
-
- 
- def show
-  render(json: @found, include: { recipes: { include: :comments } })
- end
- 
- def create
-   @found = Found.create(found_params)
-     if @found.save
-       render(json: @found)
-     else
-       render(json: @found.errors, status: :unprocessable_entity)
-     end
- end
- 
- def destroy
-   @found.destroy
-   render json: {message: "Item deleted."}
- end
- 
- def update
-   if @found.update(found_params)
-       render json: @found
-   else
-       render json: @found.errors, status: :unprocessable_entity
+   # GET /founds
+   def index
+      @founds = Found.all
+      render(json: @founds) #relationships done for you!
    end
-   end
- 
- private
 
- def set_found
-  @found = Found.find(params[:id])
- end
- 
- def found_params
- params.permit(:location_id, :lost_id)
- end
- 
+   
+   def show
+      render(json: @found, include: :locations})
+   end
+   
+   def create
+      @found = Found.create(found_params)
+      if @found.save
+         render(json: @found)
+      else
+         render(json: @found.errors, status: :unprocessable_entity)
+      end
+   end
+   
+   def destroy
+      @found.destroy
+      render json: {message: "Item deleted."}
+   end
+   
+   def update
+      if @found.update(found_params)
+         render json: @found
+      else
+         render json: @found.errors, status: :unprocessable_entity
+      end
+   end
+   
+   private
+
+   def set_found
+      @found = Found.find(params[:id])
+   end
+   
+   def found_params
+      params.permit(:location_id, :lost_id)
+   end
+   
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,47 +2,47 @@ class LocationsController < ApplicationController
     
    before_action :set_location, only: [:show, :update, :destroy]
 
- # GET /locations
- def index
-  @locations = Location.all
-  render(json: @locations, include: :founds) #relationships done for you!
- end
-
- 
- def show
-  render(json: @location, include: { recipes: { include: :comments } })
- end
- 
- def create
-   @location = Location.new(location_params)
-     if @location.save
-       render(json: @location)
-     else
-       render(json: @location.errors, status: :unprocessable_entity)
-     end
- end
- 
- def destroy
-   @location.destroy
-   render json: {message: "Item deleted."}
- end
- 
- def update
-   if @location.update(location_params)
-       render json: @location
-   else
-       render json: @location.errors, status: :unprocessable_entity
+   # GET /locations
+   def index
+      @locations = Location.all
+      render(json: @locations, include: :founds) #relationships done for you!
    end
-   end
- 
- private
 
- def set_location
-  @location = Location.find(params[:id])
- end
- 
- def location_params
- params.permit(:name)
- end
- 
+   
+   def show
+      render(json: @location, include: :founds)
+   end
+   
+   def create
+      @location = Location.new(location_params)
+      if @location.save
+         render(json: @location)
+      else
+         render(json: @location.errors, status: :unprocessable_entity)
+      end
+   end
+   
+   def destroy
+      @location.destroy
+      render json: {message: "Location deleted."}
+   end
+   
+   def update
+      if @location.update(location_params)
+         render json: @location
+      else
+         render json: @location.errors, status: :unprocessable_entity
+      end
+   end
+   
+   private
+
+   def set_location
+      @location = Location.find(params[:id])
+   end
+   
+   def location_params
+      params.permit(:name)
+   end
+
 end
