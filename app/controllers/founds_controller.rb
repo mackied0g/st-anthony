8d92902c38d6e8b20c27f54c3579d305fb7eabad
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-10-01 17:48:27 -0400

    fixed api logic

--- app/controllers/founds_controller.rb
+++ app/controllers/founds_controller.rb
@@ -1,77 +1,48 @@
 class FoundsController < ApplicationController
-    before_action :set_found, only: [:show, :edit, :update, :destroy]
     
-    # GET /founds
-    # GET /founds.json
-    def index
-       @founds = Found.all
-    end
-    
-    # GET /founds/1
-    # GET /founds/1.json
-    def show
-    end
-    
-    # GET /founds/new
-    def new
-       @found = Found.new
-    end
-    
-    # GET /founds/1/edit
-    def edit
-    end
-    
-    # POST /founds
-    # POST /founds.json
-    def create
-       @found = Found.create(found_params)
-       
-       respond_to do |format|
-          if @found.save
-             format.html { redirect_to @found, notice: 'found was successfully created.' }
-             format.json { render :show, status: :created, found: @found }
-          else
-             format.html { render :new }
-             format.json { render json: @found.errors, status: :unprocessable_entity }
-          end
-       end
-       
-    end
-    
-    # PATCH/PUT /founds/1
-    # PATCH/PUT /founds/1.json
-    def update
-       respond_to do |format|
-          if @found.update(found_params)
-             format.html { redirect_to @found, notice: 'found was successfully updated.' }
-             format.json { render :show, status: :ok, found: @found }
-          else
-             format.html { render :edit }
-             format.json { render json: @found.errors, status: :unprocessable_entity }
-          end
-       end
-       
-    end
-    
-    # DELETE /founds/1
-    # DELETE /founds/1.json
-    def destroy
-       @found.destroy
-          respond_to do |format|
-          format.html { redirect_to founds_url, notice: 'found was successfully destroyed.' }
-          format.json { head :no_content }
-       end
-    end
-    
-    private
-    
-    # Use callbacks to share common setup or constraints between actions.
-    def set_found
-       @found = Found.find(params[:id])
-    end
-    
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def found_params
-       params(:found).permit(:location_id, :lost_id)
-    end
+   before_action :set_found, only: [:show, :update, :destroy]
+
+ # GET /founds
+ def index
+  @founds = Found.all
+  render(json: @founds) #relationships done for you!
  end
+
+ 
+ def show
+  render(json: @found, include: { recipes: { include: :comments } })
+ end
+ 
+ def create
+   @found = Found.create(found_params)
+     if @found.save
+       render(json: @found)
+     else
+       render(json: @found.errors, status: :unprocessable_entity)
+     end
+ end
+ 
+ def destroy
+   @found.destroy
+   render json: {message: "Item deleted."}
+ end
+ 
+ def update
+   if @found.update(found_params)
+       render json: @found
+   else
+       render json: @found.errors, status: :unprocessable_entity
+   end
+   end
+ 
+ private
+
+ def set_found
+  @found = Found.find(params[:id])
+ end
+ 
+ def found_params
+ params.permit(:location_id, :lost_id)
+ end
+ 
+end
