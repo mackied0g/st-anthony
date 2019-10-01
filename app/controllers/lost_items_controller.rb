9c8dac94ddd88682f6d2d42f28653ede305e7e06
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-10-01 10:58:43 -0400

    added seeds

--- app/controllers/lost_items_controller.rb
+++ app/controllers/lost_items_controller.rb
@@ -1,2 +1,77 @@
 class LostItemsController < ApplicationController
-end
+    before_action :set_lost_item, only: [:show, :edit, :update, :destroy]
+    
+    # GET /lost_items
+    # GET /lost_items.json
+    def index
+       @lost_items = LostItem.all
+    end
+    
+    # GET /lost_items/1
+    # GET /lost_items/1.json
+    def show
+    end
+    
+    # GET /lost_items/new
+    def new
+       @lost_item = LostItem.new
+    end
+    
+    # GET /lost_items/1/edit
+    def edit
+    end
+    
+    # POST /lost_items
+    # POST /lost_items.json
+    def create
+       @lost_item = LostItem.new(lost_item_params)
+       
+       respond_to do |format|
+          if @lost_item.save
+             format.html { redirect_to @lost_item, notice: 'lost_item was successfully created.' }
+             format.json { render :show, status: :created, location: @lost_item }
+          else
+             format.html { render :new }
+             format.json { render json: @lost_item.errors, status: :unprocessable_entity }
+          end
+       end
+       
+    end
+    
+    # PATCH/PUT /lost_items/1
+    # PATCH/PUT /lost_items/1.json
+    def update
+       respond_to do |format|
+          if @lost_item.update(lost_item_params)
+             format.html { redirect_to @lost_item, notice: 'Lost Item was successfully updated.' }
+             format.json { render :show, status: :ok, location: @lost_item }
+          else
+             format.html { render :edit }
+             format.json { render json: @lost_item.errors, status: :unprocessable_entity }
+          end
+       end
+       
+    end
+    
+    # DELETE /lost_items/1
+    # DELETE /lost_items/1.json
+    def destroy
+       @lost_item.destroy
+          respond_to do |format|
+          format.html { redirect_to lost_items_url, notice: 'lost_item was successfully destroyed.' }
+          format.json { head :no_content }
+       end
+    end
+    
+    private
+    
+    # Use callbacks to share common setup or constraints between actions.
+    def set_lost_item
+       @lost_item = LostItem.find(params[:id])
+    end
+    
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def lost_item_params
+       params.require(:lost_item).permit(:name)
+    end
+ end
